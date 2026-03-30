97a8cd0d7d8784d711de2b76e90be4bc1b11cecd
Maikol Pirez <mpiirez13@gmail.com>
2026-03-30 17:52:16 -0300

    postgres en produccion

--- clave.py
+++ clave.py
@@ -1,10 +1,6 @@
+import os
 from cryptography.fernet import Fernet
 
-# Generamos la llave
-clave = Fernet.generate_key()
+clave = os.getenv("FERNET_KEY")
 
-# La mostramos en pantalla (esto te va a dar un chorizo de letras y números)
-print("--------------------------------------------------")
-print("Copiá esta clave y guardala bien:")
-print(clave.decode())
-print("--------------------------------------------------")
+f = Fernet(clave.encode())
