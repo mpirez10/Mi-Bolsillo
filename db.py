d410aa5b4f691cc3965eb9c5261bfe416c2af1a9
mpirez10 <mpiirez13@gmail.com>
2026-03-31 14:52:53 -0300

    Update db.py

--- db.py
+++ db.py
@@ -8,11 +8,14 @@ def get_db():
     if not database_url:
         raise Exception("DATABASE_URL no configurada")
 
+    # Normalizar prefijo si Render entrega postgres://
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
     conn = psycopg2.connect(
         database_url,
         cursor_factory=psycopg2.extras.RealDictCursor
     )
-
     return conn
 
 
